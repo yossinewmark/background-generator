bf21dcd66f7b90ba883ee00eafacf1d312681b2f
yossinewmark <yossi.newmark@gmail.com>
2020-07-14 14:01:19 -0400

    initial commit adding the background-gradient-generator app to repo

---
+++ script.js
@@ -0,0 +1,24 @@
+var css = document.querySelector('h3')
+var color1 = document.querySelector(".color1")
+var color2 = document.querySelector(".color2")
+var body = document.getElementById('gradient')
+var randomButton = document.getElementById('random-button')
+
+function setGradient() {
+    body.style.background = "linear-gradient(to right," + color1.value + "," + color2.value + ")"
+    css.textContent = body.style.background
+    // console.log(color1.value, color2.value)
+}
+
+function randomGradient() {
+    color1.value = '#' + Math.floor(Math.random()*16777215).toString(16)
+    color2.value = '#' + Math.floor(Math.random()*16777215).toString(16)
+    setGradient()
+}
+
+document.addEventListener("DOMContentLoaded", setGradient)
+color1.addEventListener("input", setGradient)
+color2.addEventListener("input", setGradient)
+
+randomButton.addEventListener('click', randomGradient)
+
